90604a3c19007954ff345c7773a4a0f4ff6cc6cc
Fullstack Data Engineer <christian@fullstack-dataengineer.dev>
2023-03-10 16:26:24 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>My first real app</h1>
+      <h2>deployed to production!</h2>
     </div>
   );
 }
